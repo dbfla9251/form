275d9a1637b6345f02e8a1f54361f67ae25e4541
dbfla9251 <dbfla0303@likelion.org>
2019-06-18 02:55:12 +0900

    first commit

--- secondproject/urls.py
+++ secondproject/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path
 import blog.views
 import portfolio.views
+import accounts.views
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -27,6 +28,10 @@ urlpatterns = [
     path('blog/new/', blog.views.new, name='new'),
     path('blog/create/', blog.views.create, name='create'),
     path('portfolio/', portfolio.views.portfolio, name='portfolio'),
+    path('accounts/signup' , accounts.views.signup,name="signup"),
+    path('accounts/login' , accounts.views.login,name="login"),
+     path('accounts/logout/', accounts.views.logout, name='logout'),
+
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
